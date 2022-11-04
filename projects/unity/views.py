259461d7e0b6fd02aa4d62b011d45e7860b715c6
최슬기 <choisk09@gmail.com>
2022-11-05 00:24:00 +0900

    ADD: added html in views.py

--- projects/unity/views.py
+++ projects/unity/views.py
@@ -3,4 +3,19 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(reqeust):
-    return HttpResponse('Welcome! hahaha')
+    return HttpResponse('''
+    <small>UNITY</small>
+    <hr>
+    <h1>June 2022</h1>
+    <div>
+        <div><samll>Email list</small><span>10,000</span></div>
+        <div><samll>New this month</small><span>501</span></div>
+        <div><samll>Unsubscribed</small><span>30</span></div>
+    <div>
+    
+    <table>
+        <tr><th>Email ID</th><th>Timestamp</th><th>Status</th><tr>
+        <tr><td>developers@konigle.com</td><td>10 minutes ago</td><td>Subscribed</td><tr>
+        <tr><td>info@konigle.com</td><td>1 hour ago</td><td>Subscribed</td><tr>
+    </table>
+  ''')
