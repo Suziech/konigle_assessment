6d3b0e0b965320756f57e26601b4b8a772564648
최슬기 <choisk09@gmail.com>
2022-11-06 22:39:14 +0900

    ADD : added class Unity in models.py

--- projects/unity/models.py
+++ projects/unity/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Unity(models.Model):
+    email = models.EmailField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
